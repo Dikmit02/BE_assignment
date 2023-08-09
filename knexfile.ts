b030a9004a5370c33d29c255da0859ce6e4e9101
DikMit02 <dikshamittal21297@gmail.com>
2023-08-10 01:23:17 +0400

    REMOVE:extra env

--- knexfile.ts
+++ knexfile.ts
@@ -9,15 +9,6 @@ module.exports = {
     connectionString:process.env.PG_CONNECTION_STRING,
     ssl:true
   },
-  
-  // {
-  //   host: process.env.DB_HOST,
-  //   port: process.env.DB_PORT,
-  //   database: process.env.DB_DATABASE,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD,
-  //   charset: 'utf8',
-  // },
   useNullAsDefault: true,
   pool: {
     min: 2,
