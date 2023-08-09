a45d67804a514317f29a5288a221e232fa9b0fb7
DikMit02 <dikshamittal21297@gmail.com>
2023-08-10 00:32:26 +0400

    ADD: Analytics

--- libs/analytics/src/transformer/analytics.ts
+++ libs/analytics/src/transformer/analytics.ts
@@ -6,7 +6,6 @@ export class AnalyticsTransformer extends Transformer {
   public availableIncludes = ['usageLimit', 'doctorDetails'];
   async transform(analytics: IAnalytics$Model): Promise<Record<string, any>> {
     return {
-      // id: analytics?.uuid,
       chartId: analytics?.chartId,
       title: analytics?.title,
       dataType: analytics?.dataType,
