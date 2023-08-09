af43d817d240979d1923a264fef12f68c5372a10
DikMit02 <dikshamittal21297@gmail.com>
2023-08-09 09:08:55 +0400

    ADD: interface and transformer

--- libs/analytics/src/interface/analytics.ts
+++ libs/analytics/src/interface/analytics.ts
@@ -1,9 +1,16 @@
 import { ObjectionModel } from '@libs/boat';
 
 export interface IAnalytics$Model extends ObjectionModel {
-  AnalyticsName?: string;
-  email?: string;
-  password?: string;
-  firstName?: string;
-  lastName?: string;
+  chartId?: string;
+  title?: string;
+  dataType?: string;
+  data?: IProductData$Model[];
+}
+
+
+interface IProductData$Model  extends ObjectionModel {
+  country?: string;
+  phising?: number;
+  awarness?: number;
+ 
 }
--- libs/analytics/src/transformer/analytics.ts
+++ libs/analytics/src/transformer/analytics.ts
@@ -4,12 +4,13 @@ import { IAnalytics$Model } from '../interface/analytics';
 export class AnalyticsTransformer extends Transformer {
   public defaultIncludes: any[] = [];
   public availableIncludes = ['usageLimit', 'doctorDetails'];
-  async transform(user: IAnalytics$Model): Promise<Record<string, any>> {
+  async transform(analytics: IAnalytics$Model): Promise<Record<string, any>> {
     return {
-      id: user.uuid,
-      firstName: user?.firstName?.replace(/\s+/g, ' ').trim(),
-      lastName: user?.lastName?.replace(/\s+/g, ' ').trim(),
-      email: user.email,
+      id: analytics?.uuid,
+      chartId: analytics?.chartId,
+      title: analytics?.title,
+      dataType: analytics?.dataType,
+      data:analytics?.data
     };
   }
 }
